78bb4f294970f4ee722de4d9ba6b8b26bc72c322
Mohit Saini <mohitsainidna@gmail.com>
2023-01-25 15:14:15 +0530

    This is a commit for web application

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 app= Flask(__name__)
 ## Loading the model
 model=pickle.load(open('housing_model.pkl','rb'))
-
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -17,3 +17,17 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.fit_transform(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
